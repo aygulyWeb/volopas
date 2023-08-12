7a555b26502e070741943470bf5d82b50b490371
aygulyWeb <augultilepova@gmal.com>
2023-08-12 18:46:54 +0500

    no-change

---
+++ js/script.js
@@ -0,0 +1,139 @@
+// Подробнее
+
+function readMore() {
+	var dots1 = document.getElementById("dots1");
+	var more1 = document.getElementById("more1");
+	var btn1 = document.getElementById("btn1");
+
+	if (dots1.style.display === "none") {
+		dots1.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more1.style.display = "none";
+	} else {
+		dots1.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more1.style.display = "inline";
+	}
+};
+function readMore2() {
+	var dots2 = document.getElementById("dots2");
+	var more2 = document.getElementById("more2");
+	var btn2 = document.getElementById("btn2");
+
+	if (dots2.style.display === "none") {
+		dots2.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more2.style.display = "none";
+	} else {
+		dots2.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more2.style.display = "inline";
+	}
+};
+
+function readMore3() {
+	var dots3 = document.getElementById("dots3");
+	var more3 = document.getElementById("more3");
+	var btn3 = document.getElementById("btn3");
+
+	if (dots3.style.display === "none") {
+		dots3.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more3.style.display = "none";
+	} else {
+		dots3.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more3.style.display = "inline";
+	}
+};
+
+function readMore4() {
+	var dots4 = document.getElementById("dots4");
+	var more4 = document.getElementById("more4");
+	var btn4 = document.getElementById("btn4");
+
+	if (dots4.style.display === "none") {
+		dots4.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more4.style.display = "none";
+	} else {
+		dots4.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more4.style.display = "inline";
+	}
+};
+function readMore5() {
+	var dots5 = document.getElementById("dots5");
+	var more5 = document.getElementById("more5");
+	var btn5 = document.getElementById("btn5");
+
+	if (dots5.style.display === "none") {
+		dots5.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more5.style.display = "none";
+	} else {
+		dots5.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more5.style.display = "inline";
+	}
+};
+function readMore6() {
+	var dots6 = document.getElementById("dots6");
+	var more6 = document.getElementById("more6");
+	var btn6 = document.getElementById("btn6");
+
+	if (dots6.style.display === "none") {
+		dots6.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more6.style.display = "none";
+	} else {
+		dots6.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more6.style.display = "inline";
+	}
+};
+function readMore7() {
+	var dots7 = document.getElementById("dots7");
+	var more7 = document.getElementById("more7");
+	var btn7 = document.getElementById("btn7");
+
+	if (dots7.style.display === "none") {
+		dots7.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more7.style.display = "none";
+	} else {
+		dots7.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more7.style.display = "inline";
+	}
+};
+function readMore8() {
+	var dots8 = document.getElementById("dots8");
+	var more8 = document.getElementById("more8");
+	var btn8 = document.getElementById("btn8");
+
+	if (dots8.style.display === "none") {
+		dots8.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more8.style.display = "none";
+	} else {
+		dots8.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more8.style.display = "inline";
+	}
+};
+function readMore9() {
+	var dots9 = document.getElementById("dots9");
+	var more9 = document.getElementById("more9");
+	var btn9 = document.getElementById("btn9");
+
+	if (dots9.style.display === "none") {
+		dots9.style.display = "inline";
+		// btn.innerHTML = "Подробнее";
+		more9.style.display = "none";
+	} else {
+		dots9.style.display = "none";
+		// btn.innerHTML = "Скрыть";
+		more9.style.display = "inline";
+	}
+};
